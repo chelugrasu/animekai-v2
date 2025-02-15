be6961cfe6c9b74c5d503eeea5b84aa5e58f97d8
Floweret Alexander <71138660+chelugrasu@users.noreply.github.com>
2025-02-15 15:22:49 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,13 +5,11 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons';
-import lenis from "vue-lenis";
 
 library.add(fas, fab)
 
 
 const app = createApp(App)
 app.use(router)
-app.use(lenis)
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
